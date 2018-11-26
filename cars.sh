4274a27ced48904088f8ea3347b81ac481d1911a
Ethan Dang <edang@chapman.edu>
2018-11-26 10:54:10 -0800

    Please enter the commit message for your changes. Lines starting

---
+++ cars.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+#! cars.sh
+#! Ethan Dang
+echo "Menu:"
+echo "1 - Add a new car"
+echo "2 - List cars"
+echo "Q - Quit program"
+read choice
+while "$choice" != "Q"
+do
+case "$choice" in
+	"1") echo "What is the year of the car?"
+		read year
+		echo "What is the make of the car?"
+		read make
+		echo "What is the model of the car?"
+		read model
+		echo "$year:$make:$model" >> My_old_cars;;
+	"2") echo "okay";;
+	"Q") echo "goodbye";;
+esac
+if [ "$choice" != "Q" ]
+then
+	echo "Menu:"
+	echo "1 - Add a new car"
+	echo "2 - List cars"
+	echo "Q - Quit program"
+	read choice
+fi
+done
