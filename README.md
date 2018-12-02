8e794909f1e6f91d1d27e577850a3b20d90d3bc3
Ethan Dang <edang@chapman.edu>
2018-12-02 17:54:49 +0000

    added all files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cars
+This directory contains the files "cars\.sh" and "My\_old\_cars".
+The program "cars\.sh" is a program that when run, gives the user the three
+commands it is able to run. The commands are listed below.
+1. "1 - add a new car"
+	* asks the user for information on the car and appends it to the file 
+"My\_old\_cars"
+2. "2 - list the cars"
+	* lists the cars in file "My\_old\_cars" from oldest to newest
+3. "Q - quit program"
+	* exits the program
+
--- cars.sh
+++ cars.sh
@@ -6,7 +6,7 @@ echo "1 - Add a new car"
 echo "2 - List cars"
 echo "Q - Quit program"
 read choice
-while "$choice" != "Q"
+while [ "$choice" != "Q" ]
 do
 case "$choice" in
 	"1") echo "What is the year of the car?"
@@ -16,7 +16,7 @@ case "$choice" in
 		echo "What is the model of the car?"
 		read model
 		echo "$year:$make:$model" >> My_old_cars;;
-	"2") echo "okay";;
+	"2") sort My_old_cars;;
 	"Q") echo "goodbye";;
 esac
 if [ "$choice" != "Q" ]
@@ -28,3 +28,7 @@ then
 	read choice
 fi
 done
+if [ "$choice" == "Q" ]
+then
+	echo "goodbye"
+fi
